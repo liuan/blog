f3a4a5fcd30a9f05ff95220bc79c76fcce6d8ab7
liuan <liuanaqi@gmail.com>
2013-10-13 21:54:55 +0800

    max two num in matrix

--- code/cpp/maxtwonuminmatrix.cpp
+++ code/cpp/maxtwonuminmatrix.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<iterator>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -22,10 +25,73 @@ int maxtwonuminmatrix(const int matrix[][4], int dimen){
     return maxsum;
 }
 
+struct Point{
+    int value;
+    int i;
+    int j;
+    Point(int va, int x, int y){
+        value = va;
+        i = x;
+        j = y;
+    }
+    Point(int va=0x80000000){
+        value = va;
+    }
+    friend ostream& operator<<(ostream& os, const Point& point);
+};
+
+ostream& operator<<(ostream& os, const Point& point){
+    return os << point.value << " " << point.i << " " << point.j;
+}
+
+int compare(const Point& left, const Point& right){
+    return left.value < right.value;
+}
+
+int maxtwonuminmatrixv2(const int matrix[][4], int dimen){
+    if(matrix == NULL || dimen <= 1)
+        return -1;
+    vector<Point> record(2*dimen);
+
+    int i = 0, j = 0;
+    for(i = 0; i < dimen; ++i){
+        for(j = 0; j < dimen; ++j){
+            if(matrix[i][j] > record[i].value){ // max num in this row
+                record[i].value = matrix[i][j];
+                record[i].i = i;
+                record[i].j = j;
+            }
+            if(matrix[i][j] > record[j+dimen].value){
+                record[j+dimen].value = matrix[i][j]; 
+                record[j+dimen].i = i;
+                record[j+dimen].j = j;
+            }
+        }
+    }
+     
+    sort(record.begin(),record.end(),compare);
+
+    int max = 0x80000000;
+    for(i = 2*dimen - 1; i >= 0; --i){
+        for(j = i-1; j >= 0; --j){
+            if(record[i].i != record[j].i && 
+                    record[i].j != record[j].j){
+                if(record[i].value + record[j].value > max){
+                    max = record[i].value + record[j].value;
+                }
+            }
+        }
+    }
+
+    return max; 
+}
+
 int main(){
     int matrix[4][4] = {3,4,5,6,
                         7,20,9,10,
                         11,12,13,14,
                         40,16,17,18};
     cout << maxtwonuminmatrix(matrix,4) << endl;
+    cout << maxtwonuminmatrixv2(matrix,4) << endl;
+    return 0;
 }
